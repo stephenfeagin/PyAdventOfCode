329844a49321fef9022930be31176cf11ae3d7e3
Stephen Feagin <sfeagin@ctrise.org>
2018-12-31 19:17:13 +0000

    Re-arrange 2018.2

--- 2018/02/day_02.py
+++ 2018/02/day_02.py
@@ -5,6 +5,7 @@
 from collections import defaultdict
 import re
 
+
 def part_1(data):
     two_times = 0
     three_times = 0
@@ -27,16 +28,3 @@ def part_2(data):
                 return re.sub(pattern="\*", repl="", string=substring)
             results.append(substring)
 
-if __name__ == "__main__":
-    if __file__ == "aoc.py":
-        INPUT = args.file
-    else:
-        INPUT = input("Puzzle input: ")
-    with open(INPUT, "r") as f:
-        DATA = [line.rstrip("\n") for line in f.readlines()]
-
-    print("Part 1:")
-    print(part_1(DATA))
-
-    print("Part 2:")
-    print(part_2(DATA))
