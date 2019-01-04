9d2efa7ed2cad37bcd3a163b63cea2f540f176ba
Stephen Feagin <sfeagin@ctrise.org>
2019-01-04 18:02:36 +0000

    Solve 2018.6.1

--- 2018/06/day_06.py
+++ 2018/06/day_06.py
@@ -1,12 +1,43 @@
+from __future__ import annotations
+from collections import defaultdict
 from dataclasses import dataclass
-from typing import Dict, List, Tuple
+from typing import DefaultDict, List, Set, Union
 import unittest
 
 
-@dataclass
+@dataclass(frozen=True)
 class Point:
+    """
+    Class for dealing with points in a Cartesian plane. Provides functions for
+    calculating Manhattan distances and finding the nearest point based on
+    Manhattan distance.
+    """
+
     x: int
     y: int
+
+    def get_distance(self, other: Point) -> int:
+        """Calculates the Manhattan distance to another point"""
+        x_dist = abs(self.x - other.x)
+        y_dist = abs(self.y - other.y)
+        return x_dist + y_dist
+
+    def find_nearest_point(self, points: List[Point]) -> Union[Point, None]:
+        """
+        Returns a Point that is the closest to self, measured by Manhattan
+        distance. If two or more points are equally close, return None.
+        """
+        distances: DefaultDict[int, List[Point]] = defaultdict(list)
+
+        for pt in points:
+            dist = self.get_distance(pt)
+            distances[dist].append(pt)
+
+        nearest = distances[min(distances.keys())]
+        if len(nearest) > 1:
+            return None
+        else:
+            return nearest[0]
 
 
 def read_input(fname: str) -> List[Point]:
@@ -19,23 +50,30 @@ def read_input(fname: str) -> List[Point]:
     return results
 
 
-def define_canvas(min_x: int, min_y: int, max_x: int, max_y: int) -> Dict[Tuple, Point]:
-    return {(x, y) for x in range(min_x, max_x + 1) for y in range(min_y, max_y + 1)}
+def define_canvas(min_x: int, min_y: int, max_x: int, max_y: int) -> Set[Point]:
+    """Returns the set of all points in a plane bounded by the arguments"""
+    return set(Point(x, y) for x in range(min_x, max_x + 1) for y in range(min_y, max_y + 1))
 
 
-def part_1(points):
-    # I know that the points with either x or y being the maximum or minimum
-    # of the group will have infinite areas, so I can eliminate them
+def part_1(points: List[Point]) -> int:
     max_x: int = max(pt.x for pt in points)
     min_x: int = min(pt.x for pt in points)
     max_y: int = max(pt.y for pt in points)
     min_y: int = min(pt.y for pt in points)
 
-    candidates: List[Point] = [
-        pt for pt in points if pt.x not in (max_x, min_x) and pt.y not in (max_y, min_y)
-    ]
+    # Get the canvas that we're working with. This helps to eliminate points
+    # with non-finite areas
+    canvas = define_canvas(min_x, min_y, max_x, max_y)
 
-    return candidates[0]
+    # Make a default dict to keep track of the areas of the points
+    pt_areas: DefaultDict[Point, int] = defaultdict(int)
+
+    for pt in canvas:
+        nearest = pt.find_nearest_point(points)
+        if nearest is not None:
+            pt_areas[nearest] += 1
+
+    return max(pt_areas.values())
 
 
 class TestDay6(unittest.TestCase):
---
+++ 2018/06/solve.py
@@ -0,0 +1,6 @@
+from day_06 import read_input, part_1
+
+points = read_input("input.txt")
+
+print("Part 1:", part_1(points))
+
